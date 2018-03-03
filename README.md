6c878ab9e9e775737ac9c6cd3bb5ebfbb5813beb
Matthew Croak <matthewcroak@Matthews-MBP.fios-router.home>
2018-03-03 16:46:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+## Mid-Module JS Project
+
+Today we are going to spend the day just creating a product. That means no tests, no specific directions...just (hopefully) joyful creation. Have fun with it. Creating amazing things is the reason we are all here.
+
+### The Goal
+
+ * Use Javascript and jQuery to manipulate the DOM
+ * Integrate with a third party API and display content on the page using AJAX requests
+ * Understand the amount of work required to build even a single feature of a website
+
+### The Problem 
+
+Your goal is to recreate a super simplified version of one of your favorite websites. There will be no peristence in your application, you will use your JS and AJAX skills to write code that runs on the client.  Your application will be what is known as a **SPA** or *Single Page Application*. This means exactly what it sounds like it does, your application will have not have separate routes. All the functionality will be happening on the same page by using JavaScript to replace and modify the HTML content in the DOM.
+
+To work with real and intersting data you will retrieve data from a third party API.  A term you may hear developers use is that your client-app will _consume_ the data (i.e the JSON) returned from the API.
+
+Your final product will be just some HTML and Javascript!
+
+#### Finding an API
+
+Once you understand which website you want to re-create, search around for that companies API. Read their documentation and make sure you can do, what you want to do with their API. In particular, we highly recommend trying to hit the API with Postman to make sure the data returned is what you expect. 
+
+**A Note on CORS** Make sure your API supports either CORS or JSONP. Google around to ensure this. If you encounter a CORS error you'll need to find another API; we haven't gotten to CORS yet so don't worry about it for now.
+
+**APIs to avoid**: Yelp, Twitter, and Facebook. They are crazy confusing. 
+
+***APIs to check out**: 
+ - City of New York Open Data: https://opendata.cityofnewyork.us/data/
+ - Public APIs:  https://github.com/toddmotto/public-apis
+
+### Some Single Page App Ideas
+
+ * Foursquare Venue search: https://foursquare.com/search
+ * Foursquare Lists browser: https://foursquare.com/jmburges/lists/created
+ * Dribble Viewer: https://dribbble.com/shots?sort=views
+
---
+++ index.js
@@ -0,0 +1,129 @@
+var button = document.getElementById("coolButton")
+var title = document.getElementById("simpsons_quote")
+var name_pic = document.getElementById("name_picture")
+var charButton = document.getElementById("characterButton")
+var input = document.getElementById("input")
+var guessed_wrong = document.getElementById('wrong')
+var characterName = ""
+var guess = []
+var newQuote = document.getElementById('newQuote')
+
+function fetchQuotes(){
+  document.getElementById("donut").style.display = "block"
+
+  guess = []
+  fetch("https://thesimpsonsquoteapi.glitch.me/quotes?count=1")
+    .then(res => res.json())
+    .then(json => showQuote(json));
+
+}
+
+function showQuote(json){
+
+  document.getElementById("donut").style.display = "none"
+  newQuote.style.display = "none"
+  var result = json[0]
+  var quote = result.quote
+  var picture = result.image
+  characterName = result.character
+  title.innerHTML = quote
+  document.querySelector('#simpson_picture').src = picture
+  document.querySelector('#name').innerHTML = characterName
+  name_pic.style.display = 'none'
+  guessed_wrong.innerHTML = ""
+  guessed_wrong.style.color = "red"
+  input.value = ""
+
+}
+
+function inputRefresh(){
+  debugger
+  document.getElementById("input").value= ''
+}
+
+
+function displayData() {
+  if (input.value.length < 1){
+    document.getElementById("dOh").style.display = "block"
+    setTimeout(function(){
+      debugger
+      document.getElementById("dOh").classList.add("reverse")
+    }, 3000)
+    setTimeout(function(){
+      debugger
+      document.getElementById("dOh").style.display = "none"
+      document.getElementById("dOh").className = ""
+    }, 4000)
+  } else {
+    if (guess.length === 2) {
+      if (input.value.toLowerCase()=== characterName.toLowerCase()) {
+        name_pic.style.display = 'unset'
+        guessed_wrong.style.color = 'green'
+        guessed_wrong.innerHTML = "RIGHT!"
+        guess = []
+
+        setTimeout(hitNewQuote, 3000)
+      } else if (input.value.split(" ")[0].toLowerCase()===characterName.split(" ")[0].toLowerCase()){
+        name_pic.style.display = 'unset'
+        guessed_wrong.style.color = 'green'
+        guessed_wrong.innerHTML = "RIGHT!"
+        setTimeout(hitNewQuote, 3000)
+        guess = []
+
+      } else {
+        debugger
+        name_pic.style.display = 'unset'
+        guessed_wrong.innerHTML = "WRONG!"
+
+        setTimeout(hitNewQuote, 3000)
+      }
+    } else {
+      if (input.value.toLowerCase()=== characterName.toLowerCase()) {
+        name_pic.style.display = 'unset'
+        guessed_wrong.style.color = 'green'
+        guessed_wrong.innerHTML = "RIGHT!"
+        setTimeout(hitNewQuote, 3000)
+        guess = []
+
+      } else if (input.value.split(" ")[0].toLowerCase()===characterName.split(" ")[0].toLowerCase()){
+        name_pic.style.display = 'unset'
+        guessed_wrong.style.color = 'green'
+        guessed_wrong.innerHTML = "RIGHT!"
+        setTimeout(hitNewQuote, 3000)
+        guess = []
+
+      } else {
+        debugger
+        name_pic.style.display = 'none'
+        guess.push(1)
+        guessed_wrong.innerHTML = "WRONG!"
+        inputRefresh()
+      }
+    }
+  }
+}
+
+function fadeOut(){
+
+  name_pic.style.animationName = "disappear"
+  name_pic.style.animationDuration = "4s"
+}
+
+function gone(){
+  name_pic.style.opacity = 0;
+}
+
+
+function reset_guess() {
+  guessed_wrong.innerHTML = ""
+}
+
+function hitNewQuote(){
+  newQuote.style.display = "unset"
+  newQuote.style.animationName = "spin"
+}
+
+title.addEventListener("load", fetchQuotes)
+button.addEventListener("click", fetchQuotes)
+charButton.addEventListener("click", displayData)
+newQuote.addEventListener("click", fetchQuotes)
